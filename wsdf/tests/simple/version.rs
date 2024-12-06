eca4d5fb343312974f72e288f56d20d6b486bb55
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-12-06 17:33:44 +0800

    lint: static constants

--- wsdf/tests/simple/version.rs
+++ wsdf/tests/simple/version.rs
@@ -4,9 +4,9 @@ version!("0.0.1", 4, 4);
 
 fn main() {
     assert_eq!(
-        plugin_version,
+        PLUGIN_VERSION,
         ['0' as i8, '.' as i8, '0' as i8, '.' as i8, '1' as i8, 0_i8]
     );
-    assert_eq!(plugin_want_major, 4_i32);
-    assert_eq!(plugin_want_minor, 4_i32);
+    assert_eq!(PLUGIN_WANT_MAJOR, 4_i32);
+    assert_eq!(PLUGIN_WANT_MINOR, 4_i32);
 }
--- wsdf/tests/simple/version2.rs
+++ wsdf/tests/simple/version2.rs
@@ -4,9 +4,9 @@ version!("5.10.01", 10, 100);
 
 fn main() {
     assert_eq!(
-        plugin_version,
+        PLUGIN_VERSION,
         ['5' as i8, '.' as i8, '1' as i8, '0' as i8, '.' as i8, '0' as i8, '1' as i8, 0_i8]
     );
-    assert_eq!(plugin_want_major, 10_i32);
-    assert_eq!(plugin_want_minor, 100_i32);
+    assert_eq!(PLUGIN_WANT_MAJOR, 10_i32);
+    assert_eq!(PLUGIN_WANT_MINOR, 100_i32);
 }
