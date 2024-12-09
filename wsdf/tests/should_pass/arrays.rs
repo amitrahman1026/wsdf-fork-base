8fb349fc115552c8ec6a52931357b26763e65327
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-12-09 21:29:15 +0800

    tests: update to new dissect traits

--- wsdf/tests/should_pass/arrays.rs
+++ wsdf/tests/should_pass/arrays.rs
@@ -3,15 +3,22 @@
 // Tests that array fields can compile
 
 use wsdf::*;
+version!("0.0.1", 4, 4);
+protocol!(ProtoFoo);
 
-#[derive(Protocol)]
-#[wsdf(decode_from = "moldudp.payload")]
+#[derive(Proto, Dissect)]
+#[wsdf(
+    proto_desc = "protocol foo",
+    proto_name = "Protocol foo",
+    proto_filter = "proto_foo",
+    decode_from = "moldudp.payload"
+)]
 struct ProtoFoo {
     bar: [u64; 10],
     baz: [Bar; 10],
 }
 
-#[derive(ProtocolField)]
+#[derive(Dissect)]
 struct Bar {
     qux: u64,
 }
